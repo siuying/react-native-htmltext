aef156a0ae59802214af616c3f0c11992a00c9a2
Francis Chong <francis@ignition.hk>
2015-03-30 01:42:01 +0800

    init commit

---
+++ index.ios.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var HtmlTextApp = React.createClass({
+  render: function() {
+    var html = `<p>Hello world</p>`
+    return (
+      <HtmlText html={html}/>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+  },
+});
+
+AppRegistry.registerComponent('HtmlTextApp', () => HtmlTextApp);
